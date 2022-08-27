e493631325f8252b6edc79a60925fd0eb2955df8
Aleksandr <aleksandr955@gmail.com>
2022-08-27 10:33:04 +0300

    tuned for venv

--- coachsite/coachsite/settings.py
+++ coachsite/coachsite/settings.py
@@ -13,7 +13,7 @@ import os.path
 import os
 import dj_database_url
 from pathlib import Path
-import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
